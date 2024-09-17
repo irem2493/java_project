ce0ed28f49b6ae9d83c060c7d30953dab1f49964
irem2493 <57314574+irem2493@users.noreply.github.com>
2024-09-17 17:13:29 +0900

    commit

--- Student.java
+++ Student.java
@@ -1,25 +1,55 @@
 package student_management_program;
 
+import java.time.LocalDate;
+
 public class Student {
     //학생 관리 프로그램을 만듭니다.
     static int no;
-    int sno;
+    String sno;
     String name;
     String tel;
-    char gender;
+    String gender;
+    int year;
+    int month;
+    int day;
     int age;
-    int birthday;
     int kor_score;
     int eng_score;
     int math_score;
     double avg;
     int rank;
 
-    public Student(String name, String tel, int birthday){
+    LocalDate now = LocalDate.now();
+    int nowYear = now.getYear();
+
+    public Student(String name, String tel, String birthday, int gender){
         no++;
-        sno = no;
+        sno = nowYear+no+"";
         this.name = name;
         this.tel = tel;
-
+        decideYMD_Age(birthday, gender);
+        decideGender(gender);
+    }
+    //생년월일 함수
+    public void decideYMD_Age(String birthday, int gender){
+        if(gender == 1 || gender == 2) year = Integer.parseInt("19"+birthday.substring(0,2));
+        else if(gender == 3 || gender == 4) year = Integer.parseInt("20"+birthday.substring(0,2));
+        month = Integer.parseInt(birthday.substring(2,4));
+        day = Integer.parseInt(birthday.substring(4,6));
+        age = nowYear - year;
+    }
+    // 성별 함수
+    public void decideGender(int gender){
+        if(gender == 1 || gender == 3) this.gender = "남자";
+        else if(gender == 2 || gender == 4) this.gender = "여자";
+        else this.gender = "";
+    }
+    //학생 정보 출력 함수
+    public void printInfo(){
+        System.out.println("학번 : "+sno);
+        System.out.println("이름 : " + name);
+        System.out.println("전화번호 : " + tel);
+        System.out.println("생년월일 : "+ year + "년 " + month + "월 " + day + "일");
+        System.out.println("나이 : " + age);
     }
 }
---
+++ Student_Management_Program.java
@@ -0,0 +1,9 @@
+package student_management_program;
+
+import java.util.Scanner;
+
+public class Student_Management_Program {
+    String name, tel, birthday;
+    Scanner sc = new Scanner(System.in);
+
+}
