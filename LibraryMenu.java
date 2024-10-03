7ede7887bdf673af0bf5a9c2e554545f267ae575
irem2493 <57314574+irem2493@users.noreply.github.com>
2024-10-03 23:58:14 +0900

    Delete LibraryMenu.java

--- LibraryMenu.java
+++
@@ -1,9 +0,0 @@
-package ribrary_program;
-
-public class LibraryMenu {
-    public static void main(String[] args) {
-        LibraryUi ui = new LibraryUi();
-        ui.intro();
-        ui.showMenu();
-    }
-}
